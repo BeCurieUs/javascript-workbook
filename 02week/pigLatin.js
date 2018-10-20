48013d58ebead47f045a2a0e322ee7c698849015
Christopher Willis <willis.christopher.a@gmail.com>
2018-10-20 12:12:57 -0500

    Added comments

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -14,6 +14,7 @@ const pigLatinMyString = (word, vowelPosition)=>{
   }
   let mainString = word.split("");
   const firstConsonants = mainString.splice(0,vowelPosition).join("");
+  //not sure if there is a single line way to do this, settled on this. Still thinking about it
   mainString = mainString.join("") + firstConsonants + "ay"
   
   return mainString;
@@ -22,43 +23,56 @@ const pigLatinMyString = (word, vowelPosition)=>{
 
 const checkValidString = (word,validString)=>{
   for(let i = 0 ; i<word.length;i++){
+    //go through every letter in the word
     let letterIsValid = 0
+    //assume the letter is invalid
     for(let j = 0; j < validString.length;j++){
+      //go through every letter in validation string
       if(word[i]==validString[j]){
+        //see if letter is a valid letter and override default
         letterIsValid = 1;
       }
     }
     if (!letterIsValid){
+      //if letter is still invalid the entire string is invalid
       return 0;
     }
   }
+  //if nothing is invalid, then everything is valid
   return 1;
 }
 const findFirstVowelPosition = (word)=>{
   const allVowels = ["a","e","i","o","u"]
   for(let i = 0 ; i<word.length;i++){
+    //go through every letter in word
     for(let j = 0; j < allVowels.length;j++){
+      //go through every vowel 
       if(word[i]==allVowels[j]){
+        //on the first vowel we find, return word index
         return i;
       }
     }
   }
+  //if no vowels found, return an unresonable index 
   return -1;
 }
 
 function pigLatin(word) {
 
-// Your code here
+  //plan
 // Check to see if input in valid
 // Find the position of the first vowel
 // String manipulation of the pig latin stuff with position
+
+//gonna use this over and over, might as well take up some memory! 
 const trimmedLowerWord = word.trim().toLowerCase();
+//debated on where to declare this, I decided to give control over the valid strigs to the logic controller
+//rather than the validation function
 const listOfValidLetters = "qwertyuiopasdfghjklzxcvbnm";
-// I decided to make a more general case for the validation where we have a set of established
-// things we know we want to be valid. It is like a DIY regular expression :D
 if (checkValidString(trimmedLowerWord,listOfValidLetters)){
+  // call this lots so decided to waste memory for processos cycles
+  // I don't want to do array compaires mutiple times
   const myFirstVowelLocation = findFirstVowelPosition(trimmedLowerWord);
-  // wasting memory for processos cycles, I don't want to do array compaires mutiple times
   if(myFirstVowelLocation == -1){
     return "No vowels in this string, please use normal English words";
   } else {
