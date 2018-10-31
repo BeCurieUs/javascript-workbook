abf6edb916a2c962a42eed77177a6d1122594804
Christopher Willis <willis.christopher.a@gmail.com>
2018-10-30 20:05:57 -0500

    Main Function Orginisation

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -32,9 +32,39 @@ function generateHint() {
   // your code here
 }
 
+
+const isValid = (guess)=>{
+
+}
+
+// user makes a guess
+// start to match the guess against the solution
+// guess has to be valid (letters, four of them, can't be empty)
+// if not, tell why
+// put guess on the board
+// check for win
+// if there is a win, tell user and reset board 
+// if there is not a win check for turns 
+// if user is on turn 10 show solution tell user and reset
+// if user isn't show hunts
+// how many letters are right place, right letters wong place
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  if(isValid(guess)){
+    board.push(guess)
+    if(checkForWin()){
+      console.log("You won");
+      resestGame();
+    }else{
+      if(board.length<10){
+        showHint();
+      }else{
+        console.log(solution);
+        console.log("You lost");
+        resestGame();
+      }
+    }
+  }
 }
 
 
